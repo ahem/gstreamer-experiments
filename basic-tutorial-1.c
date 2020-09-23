bad17a34cb1ca56fc92bcf28ba0b2fc352ff0023
Anders Hellerup Madsen <ahem@github.com>
2020-09-23 11:16:26 +0200

    formatting

--- basic-tutorial-1.c
+++ basic-tutorial-1.c
@@ -1,53 +1,51 @@
 #include <gst/gst.h>
 
-GMainLoop *main_loop;  /* GLib's Main Loop */
+GMainLoop *main_loop; /* GLib's Main Loop */
 
 /* Process messages from GStreamer */
-static gboolean handle_message (GstBus *bus, GstMessage *msg, void *user_data) {
-  switch (GST_MESSAGE_TYPE (msg)) {
-    case GST_MESSAGE_ERROR: {
-      g_print ("Error!!!\n");
-      g_main_loop_quit (main_loop);
-      return FALSE;
-    }
-    case GST_MESSAGE_EOS:
-      g_print ("End-Of-Stream reached.\n");
-      g_main_loop_quit (main_loop);
-      return FALSE;
-    default:
-      // ignore all other messages, but keep on listening
-      return TRUE;
+static gboolean handle_message(GstBus *bus, GstMessage *msg, void *user_data) {
+  switch (GST_MESSAGE_TYPE(msg)) {
+  case GST_MESSAGE_ERROR: {
+    g_print("Error!!!\n");
+    g_main_loop_quit(main_loop);
+    return FALSE;
+  }
+  case GST_MESSAGE_EOS:
+    g_print("End-Of-Stream reached.\n");
+    g_main_loop_quit(main_loop);
+    return FALSE;
+  default:
+    return TRUE;
   }
 }
 
-int main (int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
   GstElement *pipeline;
   GstBus *bus;
 
   /* Initialize GStreamer */
-  gst_init (&argc, &argv);
+  gst_init(&argc, &argv);
 
   /* Build the pipeline */
-  pipeline =
-      gst_parse_launch
-      ("playbin uri=https://www.freedesktop.org/software/gstreamer-sdk/data/media/sintel_trailer-480p.webm",
+  pipeline = gst_parse_launch(
+      "playbin "
+      "uri=https://www.freedesktop.org/software/gstreamer-sdk/data/media/sintel_trailer-480p.webm",
       NULL);
 
   /* Start playing */
-  gst_element_set_state (pipeline, GST_STATE_PLAYING);
+  gst_element_set_state(pipeline, GST_STATE_PLAYING);
 
   /* Wait until error or EOS */
-  bus = gst_element_get_bus (pipeline);
-  gst_bus_add_watch (bus, (GstBusFunc)handle_message, NULL);
+  bus = gst_element_get_bus(pipeline);
+  gst_bus_add_watch(bus, (GstBusFunc)handle_message, NULL);
 
-  main_loop = g_main_loop_new (NULL, FALSE);
-  g_main_loop_run (main_loop);
+  main_loop = g_main_loop_new(NULL, FALSE);
+  g_main_loop_run(main_loop);
 
   /* Free resources */
-  g_main_loop_unref (main_loop);
-  gst_object_unref (bus);
-  gst_element_set_state (pipeline, GST_STATE_NULL);
-  gst_object_unref (pipeline);
+  g_main_loop_unref(main_loop);
+  gst_object_unref(bus);
+  gst_element_set_state(pipeline, GST_STATE_NULL);
+  gst_object_unref(pipeline);
   return 0;
 }
--- basic-tutorial-2.c
+++ basic-tutorial-2.c
@@ -1,26 +1,26 @@
 #include <gst/gst.h>
 
-GMainLoop *main_loop;  /* GLib's Main Loop */
+GMainLoop *main_loop; /* GLib's Main Loop */
 
-static gboolean handle_message (GstBus *bus, GstMessage *msg, void *user_data) {
-  switch (GST_MESSAGE_TYPE (msg)) {
-    case GST_MESSAGE_ERROR: {
-      GError *err;
-      gchar *debug_info;
-      gst_message_parse_error (msg, &err, &debug_info);
-      g_printerr ("Error received from element %s: %s\n", GST_OBJECT_NAME (msg->src), err->message);
-      g_printerr ("Debugging information: %s\n", debug_info ? debug_info : "none");
-      g_clear_error (&err);
-      g_free (debug_info);
-      g_main_loop_quit (main_loop);
-      return FALSE;
-    }
-    case GST_MESSAGE_EOS:
-      g_print ("End-Of-Stream reached.\n");
-      g_main_loop_quit (main_loop);
-      return FALSE;
-    default:
-      return TRUE;
+static gboolean handle_message(GstBus *bus, GstMessage *msg, void *user_data) {
+  switch (GST_MESSAGE_TYPE(msg)) {
+  case GST_MESSAGE_ERROR: {
+    GError *err;
+    gchar *debug_info;
+    gst_message_parse_error(msg, &err, &debug_info);
+    g_printerr("Error received from element %s: %s\n", GST_OBJECT_NAME(msg->src), err->message);
+    g_printerr("Debugging information: %s\n", debug_info ? debug_info : "none");
+    g_clear_error(&err);
+    g_free(debug_info);
+    g_main_loop_quit(main_loop);
+    return FALSE;
+  }
+  case GST_MESSAGE_EOS:
+    g_print("End-Of-Stream reached.\n");
+    g_main_loop_quit(main_loop);
+    return FALSE;
+  default:
+    return TRUE;
   }
 }
 
@@ -30,56 +30,56 @@ int main(int argc, char *argv[]) {
   GstStateChangeReturn ret;
 
   /* Initialize GStreamer */
-  gst_init (&argc, &argv);
+  gst_init(&argc, &argv);
 
   /* Create the elements */
-  source = gst_element_factory_make ("videotestsrc", "source");
-  sink = gst_element_factory_make ("autovideosink", "sink");
+  source = gst_element_factory_make("videotestsrc", "source");
+  sink = gst_element_factory_make("autovideosink", "sink");
   filter = gst_element_factory_make("vertigotv", "filter");
 
   /* Create the empty pipeline */
-  pipeline = gst_pipeline_new ("test-pipeline");
+  pipeline = gst_pipeline_new("test-pipeline");
 
   if (!pipeline || !source || !sink) {
-    g_printerr ("Not all elements could be created.\n");
+    g_printerr("Not all elements could be created.\n");
     return -1;
   }
 
   /* Build the pipeline */
-  gst_bin_add_many (GST_BIN (pipeline), source, filter, sink, NULL);
+  gst_bin_add_many(GST_BIN(pipeline), source, filter, sink, NULL);
 
-  if (gst_element_link (source, filter) != TRUE) {
-    g_printerr ("Elements (source, filter) could not be linked.\n");
-    gst_object_unref (pipeline);
+  if (gst_element_link(source, filter) != TRUE) {
+    g_printerr("Elements (source, filter) could not be linked.\n");
+    gst_object_unref(pipeline);
     return -1;
   }
-  if (gst_element_link (filter, sink) != TRUE) {
-    g_printerr ("Elements (filter, sink) could not be linked.\n");
-    gst_object_unref (pipeline);
+  if (gst_element_link(filter, sink) != TRUE) {
+    g_printerr("Elements (filter, sink) could not be linked.\n");
+    gst_object_unref(pipeline);
     return -1;
   }
 
   /* Modify the source's properties */
-  g_object_set (source, "pattern", 0, NULL);
+  g_object_set(source, "pattern", 0, NULL);
 
   /* Start playing */
-  ret = gst_element_set_state (pipeline, GST_STATE_PLAYING);
+  ret = gst_element_set_state(pipeline, GST_STATE_PLAYING);
   if (ret == GST_STATE_CHANGE_FAILURE) {
-    g_printerr ("Unable to set the pipeline to the playing state.\n");
-    gst_object_unref (pipeline);
+    g_printerr("Unable to set the pipeline to the playing state.\n");
+    gst_object_unref(pipeline);
     return -1;
   }
 
   /* Wait until error or EOS */
-  bus = gst_element_get_bus (pipeline);
-  gst_bus_add_watch (bus, (GstBusFunc)handle_message, NULL);
+  bus = gst_element_get_bus(pipeline);
+  gst_bus_add_watch(bus, (GstBusFunc)handle_message, NULL);
 
-  main_loop = g_main_loop_new (NULL, FALSE);
-  g_main_loop_run (main_loop);
+  main_loop = g_main_loop_new(NULL, FALSE);
+  g_main_loop_run(main_loop);
 
   /* Free resources */
-  gst_object_unref (bus);
-  gst_element_set_state (pipeline, GST_STATE_NULL);
-  gst_object_unref (pipeline);
+  gst_object_unref(bus);
+  gst_element_set_state(pipeline, GST_STATE_NULL);
+  gst_object_unref(pipeline);
   return 0;
 }
